57b41ecbcc788bf1ef5a9571a3b8bebd1f507261
be5invis <belleve@typeof.net>
2021-02-15 20:08:56 -0800

    Quadify optimization

--- src/quadify/functional.ts
+++ src/quadify/functional.ts
@@ -1,57 +1,10 @@
 import { Arc } from "../derivable/interface";
-import { IVec2 } from "../point/interface";
-import { ipsErrorFitsIn, vsNumberVec2, vsQuadifyCurve } from "./vs-quadify";
+import { ipsAutoQuadify, vsNumberVec2, vsQuadifyCurve } from "./vs-quadify";
 
-function findIntersection(p1: IVec2, d1: IVec2, d2: IVec2, p2: IVec2): IVec2 | null {
-	const det = d2.x * d1.y - d2.y * d1.x;
-	const numU = (p2.y - p1.y) * d2.x - (p2.x - p1.x) * d2.y;
-	const numV = (p2.y - p1.y) * d1.x - (p2.x - p1.x) * d1.y;
-
-	if (Math.abs(det) < 1e-6) {
-		if (
-			Math.abs(numU) < 1e-12 &&
-			Math.abs(numV) < 1e-12 &&
-			numU * det <= 0 &&
-			numV * det >= 0
-		) {
-			return {
-				x: (p1.x + p2.x) / 2,
-				y: (p1.y + p2.y) / 2
-			};
-		} else {
-			return null;
-		}
-	}
-	const u = numU / det;
-	const v = numV / det;
-	if (u <= 0 || v >= 0) return null;
-	return {
-		x: p1.x + d1.x * u,
-		y: p1.y + d1.y * u
-	};
-}
-
-export function quadifyCurve(c: Arc, n: number = 1): IVec2[] | null {
-	if (n <= 0) return [];
-	if (n === 1) {
-		const m = findIntersection(c.eval(0), c.derivative(0), c.derivative(1), c.eval(1));
-		if (m) return [m];
-		else return null;
-	}
+export function quadifyCurve(c: Arc, n: number = 1) {
 	return vsQuadifyCurve(vsNumberVec2, c, n);
 }
 
-export function autoQuadifyCurve(
-	c: Arc,
-	allowError: number = 0.1,
-	maxSegments: number = 32
-): IVec2[] | null {
-	let results = null;
-	for (let s = 1; s <= maxSegments; s++) {
-		let offPoints = quadifyCurve(c, s);
-		if (!offPoints || !offPoints.length) continue;
-		if (ipsErrorFitsIn(vsNumberVec2, c, offPoints, allowError * allowError)) return offPoints;
-		results = offPoints;
-	}
-	return results;
+export function autoQuadifyCurve(c: Arc, allowError: number = 0.1, maxSegments: number = 32) {
+	return ipsAutoQuadify(vsNumberVec2, c, allowError, maxSegments);
 }
--- src/quadify/vs-quadify.ts
+++ src/quadify/vs-quadify.ts
@@ -1,10 +1,10 @@
-import { allowedNodeEnvironmentFlags } from "process";
 import { Derivable } from "../derivable";
 import { IVec2 } from "../point/interface";
 import { Offset2 } from "../point/point";
 
 export interface VectorSpace<T, X> {
 	readonly neutral: T;
+	// x + s * y
 	addScale(x: T, s: X, y: T): T;
 }
 
@@ -15,74 +15,32 @@ export interface InnerProductSpace<T, X> extends VectorSpace<T, X> {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 export function vsQuadifyCurve<V>(vs: VectorSpace<V, number>, arc: Derivable<V>, n: number) {
-	if (n < 2) throw new RangeError("vsQuadifyCurve: Must have at least 2 inner points");
-	const v = getConstantTerms(vs, arc, n);
-	const a = Array(n).fill(0);
-	const b = Array(n).fill(0);
-	const c = Array(n).fill(0);
-	getTriDiagonalABC(n, a, b, c);
-	inPlaceSolveTridiagonal(vs, n, a, b, c, v);
-	return v;
+	if (n < 1) throw new RangeError("vsQuadifyCurve: Must have at least 2 inner points");
+	let knots: V[] = [];
+	let zBefore = arc.eval(0),
+		dBefore = arc.derivative(0);
+	for (let k = 0; k < n; k++) {
+		const tAfter = (k + 1) / n;
+		const zAfter = arc.eval(tAfter);
+		const dAfter = arc.derivative(tAfter);
+		knots.push(vs.addScale(zBefore, 1 / (4 * n), dBefore));
+		knots.push(vs.addScale(zAfter, -1 / (4 * n), dAfter));
+		zBefore = zAfter;
+		dBefore = dAfter;
+	}
+
+	return knots;
 }
 
 export const vsNumber: VectorSpace<number, number> = {
 	neutral: 0,
-	addScale: (a, b, c) => a * b + c
+	addScale: (a, b, c) => a + b * c
 };
 export const vsNumberVec2: InnerProductSpace<IVec2, number> = {
 	neutral: new Offset2(0, 0),
 	addScale: (a, b, c) => new Offset2(a.x + b * c.x, a.y + b * c.y),
 	innerProduct: (a, b) => a.x * b.x + a.y * b.y
 };
-
-// Generalized, in-place Thomas algorithm
-function inPlaceSolveTridiagonal<V>(
-	vs: VectorSpace<V, number>,
-	n: number,
-	a: number[],
-	b: number[],
-	c: number[],
-	x: V[]
-) {
-	for (let j = 1; j < n; j++) {
-		const w = a[j] / b[j - 1];
-		b[j] -= w * c[j - 1];
-		x[j] = vs.addScale(x[j], -w, x[j - 1]);
-	}
-
-	x[n - 1] = vs.addScale(vs.neutral, 1 / b[n - 1], x[n - 1]);
-	for (let i = n - 1; i-- > 0; ) {
-		x[i] = vs.addScale(x[i], -c[i], x[i + 1]);
-		x[i] = vs.addScale(vs.neutral, 1 / b[i], x[i]);
-	}
-}
-
-function getTriDiagonalABC(n: number, a: number[], b: number[], c: number[]) {
-	b[0] = b[n - 1] = 1;
-	for (let j = 1; j < n - 1; j++) {
-		const mixBefore = 1 / 8,
-			mixHere = 3 / 4,
-			mixNext = 1 / 8;
-		a[j] = mixBefore;
-		b[j] = mixHere;
-		c[j] = mixNext;
-	}
-}
-
-function getConstantTerms<V>(vs: VectorSpace<V, number>, c: Derivable<V>, n: number) {
-	const constantTerms: V[] = Array(n);
-	const start = c.eval(0);
-	const end = c.eval(1);
-	const dStart = c.derivative(0);
-	const dEnd = c.derivative(1);
-	const dScale = 1 / (2 * n);
-
-	constantTerms[0] = vs.addScale(start, dScale, dStart);
-	for (let j = 1; j < n - 1; j++) constantTerms[j] = c.eval((j + 1 / 2) / n);
-	constantTerms[n - 1] = vs.addScale(end, -dScale, dEnd);
-
-	return constantTerms;
-}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -98,23 +56,14 @@ export function ipsErrorFitsIn<T>(
 	for (let j = 0; j < offPoints.length; j++) {
 		const zOffQu = offPoints[j];
 
-		const zBeforeQu = j > 0 ? vsMix(ips, zOffQu, offPoints[j - 1], 1 / 2) : c.eval(0);
-		const zAfterQu =
-			j < offPoints.length - 1 ? vsMix(ips, zOffQu, offPoints[j + 1], 1 / 2) : c.eval(1);
-
 		const tAfter = (j + 1) / offPoints.length;
 		const zAfter = c.eval(tAfter);
 		const dAfter = c.derivative(tAfter);
 
-		const dbBeforeCubic = vsScale(ips, 1 / (3 * offPoints.length), dBefore);
-		const dbAfterCubic = vsScale(ips, -1 / (3 * offPoints.length), dAfter);
-		const dbBeforeQu = vsScale(ips, 2 / 3, vsDifference(ips, zOffQu, zBeforeQu));
-		const dbAfterQu = vsScale(ips, 2 / 3, vsDifference(ips, zOffQu, zAfterQu));
-
-		if (ipsSquareDist(ips, zBefore, zBeforeQu) > squareError) return false;
-		if (ipsSquareDist(ips, zAfter, zAfterQu) > squareError) return false;
-		if (ipsSquareDist(ips, dbBeforeCubic, dbBeforeQu) > squareError) return false;
-		if (ipsSquareDist(ips, dbAfterCubic, dbAfterQu) > squareError) return false;
+		if (j % 2) {
+			const zBeforeQu = vsMix(ips, zOffQu, offPoints[j - 1], 1 / 2);
+			if (ipsSquareDist(ips, zBefore, zBeforeQu) > squareError) return false;
+		}
 
 		zBefore = zAfter;
 		dBefore = dAfter;
@@ -129,7 +78,7 @@ export function ipsAutoQuadify<T>(
 	maxSegments: number = 32
 ) {
 	let results = null;
-	for (let s = 2; s <= maxSegments; s++) {
+	for (let s = 1; s <= maxSegments; s++) {
 		let offPoints = vsQuadifyCurve(ips, c, s);
 		if (!offPoints || !offPoints.length) continue;
 		if (ipsErrorFitsIn(ips, c, offPoints, allowError * allowError)) return offPoints;
@@ -142,10 +91,6 @@ export function ipsAutoQuadify<T>(
 
 function vsDifference<T>(vs: VectorSpace<T, number>, a: T, b: T) {
 	return vs.addScale(a, -1, b);
-}
-
-function vsScale<T>(vs: VectorSpace<T, number>, p: number, a: T) {
-	return vs.addScale(vs.neutral, p, a);
 }
 
 function vsMix<T>(vs: VectorSpace<T, number>, a: T, b: T, p: number) {
